6a2cfb94701d1cef17c9f72e4efa98568091f74d
NikitaKirin <kirinnikita1406@gmail.com>
2022-10-05 11:39:33 +0500

    feat[employee_migration]: add FK for photo

--- database/migrations/2022_10_02_182158_create_employees_table.php
+++ database/migrations/2022_10_02_182158_create_employees_table.php
@@ -19,8 +19,15 @@ return new class extends Migration {
             $table->string('address')->nullable();
             $table->string('audience')->nullable();
             $table->string('additional_Information')->nullable();
-            $table->foreignId('position_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('photo_id')->nullable()
+                  ->constrained('attachments', 'id')
+                  ->nullOnDelete();
+            $table->foreignId('position_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()
+                  ->constrained()
+                  ->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
