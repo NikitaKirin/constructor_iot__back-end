d96aa6521a7ee962dcdcd8f034d955a12fe6813c
NikitaKirin <kirinnikita1406@gmail.com>
2022-10-11 13:52:16 +0500

    [laravel-telescope]: init

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,12 +10,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+    protected function schedule( Schedule $schedule ) {
+        // $schedule->command('telescope:prune --hours=48')->daily();
     }
 
     /**
@@ -23,9 +22,8 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    protected function commands() {
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
