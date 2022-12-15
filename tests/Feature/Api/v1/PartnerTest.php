5aac252e48065e7c1da5a9fa90bbabe75ce05378
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-15 17:18:29 +0500

    check ci

--- tests/Feature/Api/v1/PartnerTest.php
+++ tests/Feature/Api/v1/PartnerTest.php
@@ -13,25 +13,30 @@ class PartnerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testPartnersIndex() {
-
+    public function testPartnersIndex()
+    {
         $testPartner = Partner::factory(10)->create()->first();
 
         $response = $this->get(route('partners.index'));
 
         $response->assertOk();
 
-        $response->assertJson(fn( AssertableJson $json ) => $json->has('meta')
-                                                                 ->has('links', 4)
-                                                                 ->has('partners', 5)
-                                                                 ->has('partners.0',
-                                                                     fn( $json ) => $json->where('id', $testPartner->id)
-                                                                                         ->where('title',
-                                                                                             $testPartner->title)
-                                                                                         ->where('description',
-                                                                                             $testPartner->description)
-                                                                                         ->etc()
-                                                                 )
+        $response->assertJson(fn(AssertableJson $json) => $json->has('meta')
+            ->has('links', 4)
+            ->has('partners', 5)
+            ->has(
+                'partners.0',
+                fn($json) => $json->where('id', $testPartner->id)
+                    ->where(
+                        'title',
+                        $testPartner->title
+                    )
+                    ->where(
+                        'description',
+                        $testPartner->description
+                    )
+                    ->etc()
+            )
         );
     }
 }
