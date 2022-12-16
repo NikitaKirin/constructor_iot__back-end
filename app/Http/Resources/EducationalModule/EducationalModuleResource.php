95885f80e7e67045ad6c7d978ad4166a49e833d7
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-16 19:05:22 +0500

    fix[educational_module_api]:fix get disciplines for current professional trajectory

--- app/Http/Resources/EducationalModule/EducationalModuleResource.php
+++ app/Http/Resources/EducationalModule/EducationalModuleResource.php
@@ -14,20 +14,20 @@ class EducationalModuleResource extends JsonResource
     public static $wrap = 'educational_module';
 
     /**
-     * @param Request $request
+     * @param  Request  $request
      * @return array
      */
-    public function toArray( $request ): array {
+    public function toArray($request): array
+    {
         return [
-            'id'           => $this->id,
-            'title'        => $this->title,
+            'id' => $this->id,
+            'title' => $this->title,
             'choice_limit' => $this->choice_limit,
-            'is_spec'      => $this->is_spec,
+            'is_spec' => $this->is_spec,
             //            'disciplines_count'            => $this->disciplines_count,
             //            'educational_directions_count' => $this->educational_directions_count,
             //'semesters_count'              => $this->semesters_count,
-            //            'disciplines'  => $this->whenLoaded('disciplines', fn() => $this->getDisciplines($request)),
-            'disciplines'  => DisciplineResource::collection($this->whenLoaded('disciplines'))
+            'disciplines' => $this->whenLoaded('disciplines', fn() => $this->getDisciplines($request)),
 
 
             /*'educationalDirections' => EducationalDirectionResourceCollection::collection($this->whenLoaded('educationalDirections')),*/
@@ -37,15 +37,20 @@ class EducationalModuleResource extends JsonResource
 
     /**
      * Get the list of disciplines
-     * @param Request $request
+     * @param  Request  $request
      * @return AnonymousResourceCollection
      */
-    private function getDisciplines( Request $request ): AnonymousResourceCollection {
-        if ( ($professionalTrajectoryId = $request->input('professionalTrajectoryId')) && $this->is_spec ) {
-            return DisciplineResource::collection($this->disciplines()->whereHas('professionalTrajectories',
-                function ( Builder $query ) use ( $professionalTrajectoryId ) {
-                    return $query->where('id', '=', $professionalTrajectoryId);
-                })->orderBy('title')->get());
+    private function getDisciplines(Request $request): AnonymousResourceCollection
+    {
+        if (($professionalTrajectoryId = $request->input('professionalTrajectoryId')) && $this->is_spec) {
+            return DisciplineResource::collection(
+                $this->disciplines()->whereHas(
+                    'professionalTrajectories',
+                    function (Builder $query) use ($professionalTrajectoryId) {
+                        return $query->where('id', '=', $professionalTrajectoryId);
+                    }
+                )->with('professionalTrajectories')->orderBy('title')->get()
+            );
         }
         return DisciplineResource::collection($this->disciplines);
     }
