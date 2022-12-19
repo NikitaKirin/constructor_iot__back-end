845f9d6551121c88990220369f9b744d8f165ac6
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-19 15:43:43 +0500

    feat[realization]: add factory

--- database/factories/RealizationFactory.php
+++ database/factories/RealizationFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class RealizationFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->unique()->name,
+            'user_id' => User::factory(),
         ];
     }
 }
