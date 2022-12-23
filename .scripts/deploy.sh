2064fa82c6939fd1ab09d86ed602aa2df0d04efc
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-23 13:15:25 +0500

    ci-cd[.scripts/deploy.sh]: add deploy.sh for server

---
+++ .scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin main
+
+#  Install composer dependencies
+~/constructor-iot-backend.na4u.ru/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Run migration
+php artisan migrate
+
+# Clear the old cache
+php artisan clear-compiled
+# Recreate cache
+php artisan optimize
+
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
