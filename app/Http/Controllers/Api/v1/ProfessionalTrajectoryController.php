e9dec776c8fb916cc22fa937f5ec90bfa73ad15e
NikitaKirin <kirinnikita1406@gmail.com>
2023-05-09 20:26:19 +0500

    task: reorganise api

--- app/Http/Controllers/Api/v1/ProfessionalTrajectoryController.php
+++ app/Http/Controllers/Api/v1/ProfessionalTrajectoryController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ProfessionalTrajectory\IndexProfessionalTrajectoryRequest;
 use App\Http\Resources\ProfessionalTrajectory\ProfessionalTrajectoryResource;
 use App\Http\Resources\ProfessionalTrajectory\ProfessionalTrajectoryResourceCollection;
+use App\Models\EducationalProgram;
 use App\Models\ProfessionalTrajectory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProfessionalTrajectoryController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request, EducationalProgram $educationalProgram) {
         $withProfessions = $request->boolean('loadProfessions');
-        $professionalTrajectoryQuery = ProfessionalTrajectory::query()->with(['icons']);
+        $professionalTrajectoryQuery = ProfessionalTrajectory::query()->with(['icons'])
+            ->whereHas('courseAssemblies.discipline.educationalPrograms',
+                fn(Builder $builder) => $builder->where('id', $educationalProgram->id)
+            );
         if ($withProfessions) {
             $professionalTrajectoryQuery = $professionalTrajectoryQuery->with(['professions']);
         }
@@ -23,9 +27,9 @@ class ProfessionalTrajectoryController extends Controller
     public function show(ProfessionalTrajectory $professionalTrajectory, Request $request) {
         $professionalTrajectory->load(['icons']);
         if ($request->boolean('loadCourseAssembliesCount')) {
-           $professionalTrajectory->loadCount('courseAssemblies');
+            $professionalTrajectory->loadCount('courseAssemblies');
         }
-        if ($request->boolean('loadProfessions')){
+        if ($request->boolean('loadProfessions')) {
             $professionalTrajectory->load(['professions']);
         }
         return new ProfessionalTrajectoryResource($professionalTrajectory);
--- routes/api/v1/professionalTrajectory.php
+++ routes/api/v1/professionalTrajectory.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Api\v1\ProfessionalTrajectoryController;
 
-Route::as('professionalTrajectories.')->group(function () {
+Route::as('professionalTrajectories.')->prefix('educationalPrograms/')->group(function () {
 
-    Route::get('professionalTrajectories', [ProfessionalTrajectoryController::class, 'index'])
+    Route::get('{educationalProgram}/professionalTrajectories', [ProfessionalTrajectoryController::class, 'index'])
          ->name('index');
 
     Route::get('professionalTrajectories/{professionalTrajectory}/{courseAssembliesCount?}/{vacanciesCount?}', [ProfessionalTrajectoryController::class, 'show'])
