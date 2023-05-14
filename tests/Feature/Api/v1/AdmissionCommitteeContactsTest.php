31ed1b6211971ad69f3648f080926ece7f6e3dec
NikitaKirin <kirinnikita1406@gmail.com>
2023-05-14 18:14:49 +0500

    task: add new tests for contacts block

--- tests/Feature/Api/v1/AdmissionCommitteeContactsTest.php
+++ tests/Feature/Api/v1/AdmissionCommitteeContactsTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Api\v1;
 
 use App\Models\AdmissionCommitteeContactsBlock;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
@@ -12,24 +11,38 @@ class AdmissionCommitteeContactsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testAdmissionCommitteeContactsIndex() {
+    public function testAdmissionCommitteeContactsIndexAssertJsonStructure() {
+        $response = $this->get(route('admissionCommitteeContactsBlock.index'));
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'admission_committee_contacts_block' => [
+                'id',
+                'address',
+                'phone_number',
+                'email',
+                'institute',
+            ],
+        ]);
+    }
+
+    public function testAdmissionCommitteeContactsIndexAssertJsonValue() {
 
         $admissionCommitteeContactsBlock = AdmissionCommitteeContactsBlock::all()
-                                                                          ->first();
+            ->first();
 
         $response = $this->get(route('admissionCommitteeContactsBlock.index'));
 
         $response->assertOk();
 
-        $response->assertJson(fn( AssertableJson $json ) => $json->has('admission_committee_contacts_block',
-            fn( $json ) => $json->where('id', $admissionCommitteeContactsBlock->id)
-                                ->where('address',
-                                    $admissionCommitteeContactsBlock->address)
-                                ->where('phone_number',
-                                    $admissionCommitteeContactsBlock->phone_number)
-                                ->where('email',
-                                    $admissionCommitteeContactsBlock->email)
-                                ->where('institute', $admissionCommitteeContactsBlock->institute->abbreviation)
+        $response->assertJson(fn(AssertableJson $json) => $json->has('admission_committee_contacts_block',
+            fn($json) => $json->where('id', $admissionCommitteeContactsBlock->id)
+                ->where('address',
+                    $admissionCommitteeContactsBlock->address)
+                ->where('phone_number',
+                    $admissionCommitteeContactsBlock->phone_number)
+                ->where('email',
+                    $admissionCommitteeContactsBlock->email)
+                ->where('institute', $admissionCommitteeContactsBlock->institute->abbreviation)
         ));
 
     }
