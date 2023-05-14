aa745a400bb57e6a0eff08a6c083320810f7196f
NikitaKirin <kirinnikita1406@gmail.com>
2023-05-14 18:08:03 +0500

    task: add new tests for partners and partner's courses

--- tests/Feature/Api/v1/PartnerTest.php
+++ tests/Feature/Api/v1/PartnerTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Feature\Api\v1;
 
+use App\Models\Course;
 use App\Models\Partner;
-use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
@@ -13,9 +12,23 @@ class PartnerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testPartnersIndex()
-    {
-        $testPartner = Partner::factory(10)->create()->sortBy('title')->first();
+    public function testPartnerIndexAssertJsonStructure() {
+        Partner::factory(5)->create()->sortBy('title')->first();
+        $response = $this->get(route('partners.index'));
+        $response->assertOk()->assertJsonStructure([
+            'partners' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'site_link',
+                    'logo',
+                ],
+            ],
+        ]);
+    }
+
+    public function testPartnersIndexAssertJsonValue() {
+        $testPartner = Partner::factory(5)->create()->sortBy('title')->first();
 
         $response = $this->get(route('partners.index'));
 
@@ -25,16 +38,119 @@ class PartnerTest extends TestCase
             fn(AssertableJson $json) => $json->has(
                 'partners.0',
                 fn($json) => $json->where('id', $testPartner->id)
-                    ->where(
-                        'title',
-                        $testPartner->title
-                    )
-                    ->where(
-                        'site_link',
-                        $testPartner->site_link
-                    )
+                    ->where('title', $testPartner->title)
+                    ->where('site_link', $testPartner->site_link)
                     ->etc()
             )
         );
+    }
+
+    public function testPartnersCoursesIndexAsserJsonStructure() {
+        Partner::factory(5)->hasCourses(4)->create()->sortBy('title')->first();
+        $response = $this->get(route('partners.courses.index'));
+        $response->assertOk()->assertJsonStructure([
+            'courses' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'description',
+                    'realization',
+                    'professional_trajectories' => [
+                        '*' => [
+                            'id',
+                            'title',
+                            'description',
+                            'abbreviated_name',
+                            'color',
+                            'course_assembly_evaluation',
+                        ],
+                    ],
+                    'partner'                   => [
+                        'id',
+                        'title',
+                        'site_link',
+                        'logo',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testPartnersCoursesIndexAssertJsonValue() {
+        Partner::factory()->hasCourses()->create();
+        $course = Course::first();
+        $response = $this->get(route('partners.courses.index'));
+        $response->assertJson(fn(AssertableJson $json) => $json->has(
+            'courses.0',
+            fn(AssertableJson $json) => $json->where('id', $course->id)
+                ->where('title', $course->title)
+                ->where('description', $course->description)
+                ->where('realization', $course->realization->title)
+                ->has(
+                    'partner',
+                    fn(AssertableJson $json) => $json->where('id', $course->partner->id)
+                        ->where('title', $course->partner->title)
+                        ->where('site_link', $course->partner->site_link)
+                        ->etc()
+                )
+                ->etc()
+        )->etc()
+        );
+    }
+
+    public function testPartnerCoursesShowAssertJsonStructure() {
+        $course = Course::factory()->for(Partner::factory())->create()->first();
+        $response = $this->get(route('partners.courses.show', $course));
+        $response->assertOk()->assertJsonStructure([
+                'course' => [
+                    'id',
+                    'title',
+                    'description',
+                    'realization',
+                    'professional_trajectories' => [
+                        '*' => [
+                            'id',
+                            'title',
+                            'description',
+                            'abbreviated_name',
+                            'color',
+                            'course_assembly_evaluation',
+                        ],
+                    ],
+                    'partner'                   => [
+                        'id',
+                        'title',
+                        'site_link',
+                        'logo',
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function testPartnerCoursesShowAssertJsonValue() {
+        $course = Course::factory()->for(Partner::factory())->create()->first();
+        $response = $this->get(route('partners.courses.show', $course));
+        $response->assertOk()->assertJson(fn(AssertableJson $json) => $json->has(
+            'course',
+            fn(AssertableJson $json) => $json->where('id', $course->id)
+                ->where('title', $course->title)
+                ->where('description', $course->description)
+                ->where('realization', $course->realization->title)
+                ->has('partner',
+                    fn(AssertableJson $json) => $json->where('id', $course->partner->id)
+                        ->where('title', $course->partner->title)
+                        ->where('site_link', $course->partner->site_link)
+                        ->etc()
+                )
+                ->etc()
+        )
+        );
+    }
+
+    public function testNonExistingPartnersCoursesShow() {
+        Course::factory()->create();
+        $course_id = Course::latest()->first()->id + 1;
+        $this->get(route('partners.courses.show', ['course' => $course_id]))->assertStatus(404);
     }
 }
